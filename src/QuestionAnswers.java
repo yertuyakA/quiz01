6b613f21d35a9d8c8d19517f046e1f5cd9eba3f3
Laura_Zhumakanova <laura5891@gmail.com>
2020-03-05 21:57:13 +0600

    Done

---
+++ src/QuestionAnswers.java
@@ -0,0 +1,6 @@
+import java.util.HashMap;
+
+public class QuestionAnswers {
+    private HashMap<String,String> scoreAnswerMap = new HashMap<>();
+
+}
--- src/Theme.java
+++ src/Theme.java
@@ -2,9 +2,13 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Theme {
-    List<String> questionList = new ArrayList<>();
+    private List<String> questionList = new ArrayList<>();
 
     public void checkAnswer() {
 
     }
+    void setQuestionList(List<String> questionList) {
+        this.questionList = questionList;
+    }
+
 }
