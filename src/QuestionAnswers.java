73e4278c4df7bd7671333908260a6015d38a90d8
Laura_Zhumakanova <laura5891@gmail.com>
2020-03-05 22:18:34 +0600

    Done

--- src/QuestionAnswers.java
+++ src/QuestionAnswers.java
@@ -3,4 +3,11 @@ import java.util.HashMap;
 public class QuestionAnswers {
     private HashMap<String,String> scoreAnswerMap = new HashMap<>();
 
+    void setScoreAnswerMap(HashMap<String,String> scoreAnswerMap){
+        this.scoreAnswerMap = scoreAnswerMap;
+    }
+
+    public HashMap<String, String> getScoreAnswerMap() {
+        return scoreAnswerMap;
+    }
 }
--- src/Theme.java
+++ src/Theme.java
@@ -11,7 +11,7 @@ public class Theme {
         this.questionList = questionList;
     }
 
-    List<String> getQuestionList() {
+    public List<String> getQuestionList() {
         return questionList;
     }
 
