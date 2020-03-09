de85e08fe0322cdb9ec52c5ced3740e0804d5ac2
Laura_Zhumakanova <laura5891@gmail.com>
2020-03-09 13:33:52 +0600

    add user into list users

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,35 @@
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 /****************************
  * @author a.yertuyak
  * @since 07.03.2020
  ***************************/
 public class Main {
     public static void main(String[] args) {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/quiz","root", ""); //quiz - наша база
+            Statement stmt = con.createStatement();
+            ResultSet rs = stmt.executeQuery("select*from users");
 
+            List<User> users = new ArrayList<>();
+            while (rs.next()){
+                User user = new User();
+                user.setId(rs.getInt(1));//Сюда закидывается id Юзера
+                user.setNickname(rs.getString(2));
+                user.setPassword(rs.getString(3));
+                user.setLogin(rs.getString(4));
+                user.setMaxScore(rs.getInt(5));
+
+                users.add(user);
+            }
+            System.out.println(users);
+
+
+        } catch (ClassNotFoundException | SQLException e) {
+            System.out.println(e);
+        }
     }
 }
--- src/User.java
+++ src/User.java
@@ -4,7 +4,7 @@ import java.util.List;
  * Created by MSovet on 05.03.2020.
  */
 public class User {
-
+    private int id;
     private String login;
     private String password;
     private String nickname;
@@ -14,6 +14,7 @@ public class User {
     void setLogin(String login) {
         this.login = login;
     }
+    void setId(int id) {this.id = id;}
     void setPassword(String password){this.password = password;}
     void setNickname(String nickname){this.nickname = nickname;}
     void setMaxScore(int maxScore){this.maxScore = maxScore;}
@@ -22,6 +23,20 @@ public class User {
     }
 
     String getLogin () {return login;}
+    int getId() {return id;}
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", login='" + login + '\'' +
+                ", password='" + password + '\'' +
+                ", nickname='" + nickname + '\'' +
+                ", maxScore=" + maxScore +
+                ", scorelist=" + scorelist +
+                '}';
+    }
+
     String getPassword () {return password;}
     String getNickname () {return nickname;}
     int getMaxScore () {return maxScore;}
