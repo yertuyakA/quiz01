cf8abd827f9ad41dcb0785ed63d9bff36a498107
Issatay <issatai84@gmail.com>
2020-03-09 12:37:57 +0500

    Rating

--- src/User.java
+++ src/User.java
@@ -9,6 +9,7 @@ public class User {
     private String password;
     private String nickname;
     private int maxScore;
+    private int id;
     private List<Integer> scorelist;
 
     void setLogin(String login) {
@@ -17,6 +18,7 @@ public class User {
     void setPassword(String password){this.password = password;}
     void setNickname(String nickname){this.nickname = nickname;}
     void setMaxScore(int maxScore){this.maxScore = maxScore;}
+    void setId(int id){this.id = id;}
     void setScorelist(List <Integer> scorelist) {
         this.scorelist = scorelist;
     }
@@ -25,8 +27,14 @@ public class User {
     String getPassword () {return password;}
     String getNickname () {return nickname;}
     int getMaxScore () {return maxScore;}
+    int getId(){return id;}
 
     public List<Integer> getScorelist() {
         return scorelist;
+    }
+
+    @Override
+    public String toString() {
+        return "Id"+this.id +"nickname" + this.nickname;
     }
 }
