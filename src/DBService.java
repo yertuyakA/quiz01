e44fda7b232edae46407c576df2345f01ea54af2
Laura_Zhumakanova <laura5891@gmail.com>
2020-03-14 20:50:42 +0600

    change MainService.java: change method login(); add method createUser();

---
+++ src/DBService.java
@@ -0,0 +1,59 @@
+
+
+import java.sql.*;
+
+public class DBService {
+    Connection con;
+    Statement stmt;
+
+    public DBService() {
+        try {
+            Class.forName("com.mysql.jdbc.Driver");
+            con = DriverManager.getConnection("jdbc:mysql://localhost:3306/quiz", "root", ""); //quiz - наша база
+            stmt = con.createStatement();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+    public boolean isRegistered(String login, String password) throws
+        SQLException {
+            PreparedStatement pstm = con.prepareStatement("select * from users where login = ? and password = ?");
+            pstm.setString(1, login);
+            pstm.setString(2, password);
+            ResultSet rs = pstm.executeQuery();
+            if(!rs.next()){
+                System.out.println("User не найден");
+            }else{
+                return true;
+            }
+
+            return false;
+            // select * from users where login = 'Qwer' and password = 'Qwerty123'
+        }
+
+    public boolean loginExist(String writeLogin) throws SQLException {
+        PreparedStatement pstm = con.prepareStatement("select * from users where login = ?");
+        pstm.setString(1, writeLogin);
+        ResultSet log = pstm.executeQuery();
+        if(log.next()) {
+            System.out.println("Логин уже существует.");
+        } else {
+            return false;
+        }
+        return true;
+    }
+
+
+    public void write(String writeNickname, String writePassword, String writeLogin) throws
+        SQLException {
+            String sql = "INSERT INTO users (nickname , password, login) values(?, ?, ?)";
+           PreparedStatement pstm = con.prepareStatement ("INSERT INTO users (nickname, password, login) values(?, ?, ?)");
+            pstm.setString(1, writeNickname);
+            pstm.setString(2, writePassword);
+            pstm.setString(3, writeLogin);
+            pstm.execute();
+        }
+
+
+}
+
---
+++ src/MainService.java
@@ -0,0 +1,54 @@
+import java.sql.SQLException;
+import java.util.Scanner;
+
+public class MainService {
+    DBService dBService;
+
+    public void run() throws SQLException {
+        dBService = new DBService();
+        // здесь будет наша логика
+
+        login();
+        //createUser();
+    }
+
+        public void login() throws SQLException {
+            Scanner sc = new Scanner(System.in);
+            System.out.println("Введите логин");
+            String login = sc.nextLine();
+            System.out.println("Введите пароль");
+            String password = sc.nextLine();
+            if (dBService.isRegistered(login, password)) {
+                System.out.println("Вы вошли в систему");
+            } else {
+                System.out.println("Не хотите зарегистрироваться?");
+                if (sc.nextLine().equals("да")) {
+                    createUser();
+                } else {
+                    System.out.println("Для повторного входа введите любой символ");
+                    if(!sc.nextLine().equals("")){
+                        login();
+                }
+            }
+        }
+  }
+    public void createUser() throws SQLException {
+        System.out.println("Nickname:");
+        Scanner in = new Scanner(System.in);
+        String writeNickname = in.nextLine();
+        System.out.println("Логин:");
+        String writeLogin = in.nextLine();
+        System.out.println("Пароль:");
+        String writePassword = in.nextLine();
+        //проверка на уникальность в БД
+        if(dBService.loginExist(writeLogin)) {
+
+        } else {
+            dBService.write(writeNickname, writePassword, writeLogin);
+            System.out.println("Вы успешно зарегистрированы!");
+            login();
+        }
+    }
+
+
+}
