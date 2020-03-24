2ca0b43eb536a79276f6b18c4fa544358d862ce4
Laura_Zhumakanova <laura5891@gmail.com>
2020-03-24 14:10:36 +0600

    fix error: change path for main.fxml in Main.java.

--- src/com/quiz/Main.java
+++ src/com/quiz/Main.java
@@ -10,6 +10,8 @@ import java.net.URL;
 import java.sql.SQLException;
 
 
+
+
 public class Main extends Application {
 
     public static void main(String[] args) {
@@ -27,7 +29,7 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) throws Exception {
         FXMLLoader loader = new FXMLLoader();
-        URL xmlUrl = getClass().getResource("com/quiz/main.fxml");
+        URL xmlUrl = getClass().getResource("main.fxml");
         loader.setLocation(xmlUrl);
         Parent root = loader.load();
 
